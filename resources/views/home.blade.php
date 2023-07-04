ffc927f1818cfe6d89d5b0871df885e208531cb3
Antonio D'Angelo <reapercsp@gmail.com>
2023-07-04 16:57:08 +0200

    fix pages add button edit

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,17 +1,15 @@
-
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>Laravel</title>
+        <title>Laravel Invoices</title>
         @vite('resources/js/app.js')
     </head>
     <body>
         <h1 class="text-center">Le mie fatture</h1>
         <div class="container text-center py-2">
-            <a class="btn btn-primary" href="/invoices">show the invoices</a>
+            <a class="btn btn-primary" href="/invoices">Show the Invoices</a>
         </div>
-        {{-- <img src="{{ Vite::asset('resources/img/picsum30.jpg') }}" alt=""> --}}
     </body>
 </html>
