72ebcf6cd9ce095513cd463d60fb10d42627a487
DavideFarci <yank92.df@gmail.com>
2023-07-04 17:11:33 +0200

    add trashed structure and fix route in create

--- resources/views/Invoices/trashed.blade.php
+++ resources/views/Invoices/trashed.blade.php
@@ -17,4 +17,38 @@
             </div>
         </div>
     </div>
+    <table class="table">
+        <thead>
+          <tr>
+            <th scope="col">Number</th>
+            <th scope="col">Paid</th>
+            <th scope="col">Issue date</th>
+            <th scope="col">Collection date</th>
+            <th scope="col">Buyer name</th>
+            <th scope="col">Buyer surname</th>
+            <th scope="col">Buyer street</th>
+            <th scope="col">Amount</th>
+            <th scope="col">Dettagli</th>
+            <th scope="col">Edit</th>
+          </tr>
+        </thead>
+        
+        <tbody>
+          @foreach($trashedInvoices as $invoice)
+            <tr>
+              <th scope="row">{{$invoice->number}}</th>
+              <td>{{$invoice->paid ? 'yes' : 'no'}}</td>
+              <td>{{$invoice->issue_date}}</td>
+              <td>{{$invoice->collection_date}}</td>
+              <td>{{$invoice->buyer_name}}</td>
+              <td>{{$invoice->buyer_surname}}</td>
+              <td>{{$invoice->buyer_street}}</td>
+              <td>{{$invoice->amount}} $</td>
+              <td><a href="{{route('invoices.show', ['invoice' => $invoice->id])}}" class="btn btn-primary">Mostra</a></td>
+              <td><a href="{{route('invoices.edit', ['invoice' => $invoice->id])}}" class="btn btn-warning">Edit</a></td>
+            </tr>
+          @endforeach
+        </tbody>
+      </table>
+      {{ $trashedInvoices->links() }}
 @endsection
