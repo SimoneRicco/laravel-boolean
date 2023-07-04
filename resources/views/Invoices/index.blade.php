1ce4f1e999f10ab4ce807e3e5fd8b77b5aed79af
Lucacasamassima2001 <casamassimaluca723@gmail.com>
2023-07-04 15:54:57 +0200

    index

--- resources/views/Invoices/index.blade.php
+++ resources/views/Invoices/index.blade.php
@@ -1,4 +1,32 @@
 @extends('layout.base')
 @section('content')
-    
+
+{{-- @foreach($invoices as $invoice) --}}
+<table class="table">
+    <thead>
+      <tr>
+        <th scope="col">id</th>
+        <th scope="col">Paid</th>
+        <th scope="col">Issue_date</th>
+        <th scope="col">Collection_date</th>
+        <th scope="col">Buyer_name</th>
+        <th scope="col">Buyer_surname</th>
+        <th scope="col">Buyer_street</th>
+        <th scope="col">Amount</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <th scope="row">1</th>
+        <td>Mark</td>
+        <td>Otto</td>
+        <td>@mdo</td>
+        <td>@mdo</td>
+        <td>@mdo</td>
+        <td>@mdo</td>
+        <td>@mdo</td>
+      </tr>
+    </tbody>
+  </table>
+  {{-- @endforeach --}}
 @endsection
