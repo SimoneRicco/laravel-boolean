0cdeb277fc10307567becb6c065dd8b661cdcd53
Lucacasamassima2001 <casamassimaluca723@gmail.com>
2023-07-04 17:59:31 +0200

    fix controller

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\InvoiceController;
+use App\Http\Controllers\Guest\InvoiceController;
 use App\Http\Controllers\Guest\PageController;
 
 /*
